6d509c3a171513b274eb16cd48f81685db31b69f
walidramadan12 <107707564+walidramadan12@users.noreply.github.com>
2023-07-27 20:34:48 +0300

    added script

---
+++ assets/js/login.js
@@ -0,0 +1,67 @@
+const fighters = [
+    "🐉",
+    "🐥",
+    "🐊",
+    "💩",
+    "🦍",
+    "🐢",
+    "🐩",
+    "🦭",
+    "🦀",
+    "🐝",
+    "🤖",
+    "🐘",
+    "🐸",
+    "🕷",
+    "🐆",
+    "🦕",
+    "🦁",
+  ]
+  
+  // function test(){
+  //   for (let i = 0; i<fighters.length; i+=3){
+  //     console.log(fighters[i])
+  //   }
+  // }
+  
+  let stageEl = document.getElementById("stage")
+  let fightButtonEl = document.getElementById("fightButton")
+  fightButtonEl.addEventListener("click",function(){
+  
+    let char01 =  Math.floor(Math.random(fighters)*fighters.length)
+    let char02 =  Math.floor(Math.random(fighters)*fighters.length)
+    let diff = 0
+    // stageEl.textContent = fighters[char01] +  "VS" + fighters[char02]
+    stageEl.innerHTML = `${fighters[char01]} VS ${fighters[char02]}`
+  
+    // console.log(char01)
+    // console.log(fighters[3])
+    // console.log(fighters[char01])
+    // console.log(fighters[char02])
+  
+    // fightButtonEl.style.backgroundColor="#fff"
+    if(fighters[3] == fighters[char01] || fighters[3] == fighters[char02]){
+      stageEl.innerHTML += `<br> ${fighters[3]} is the winner`
+    }else{
+      if(char01 > char02){
+        diff = char01 - char02
+        stageEl.innerHTML += `<br> ${fighters[char01]} win by ${diff} points`
+        if(diff>9){
+          fightButtonEl.style.backgroundColor="#ff5"
+        }else{
+          fightButtonEl.style.backgroundColor="#fff"
+        }
+      }else if(char01 < char02){
+        diff = char02 - char01
+        stageEl.innerHTML += `<br> ${fighters[char02]} win by ${diff} points`
+        if(diff>9){
+          fightButtonEl.style.backgroundColor="#ff5"
+        }else{
+          fightButtonEl.style.backgroundColor="#fff"
+        }
+      }else{
+        stageEl.innerHTML += `<br> Tie Game`
+        fightButtonEl.style.backgroundColor="#f0f"
+      }
+    }
+  })
